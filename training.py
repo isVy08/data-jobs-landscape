e3d6a23900dd8850aba5f3bee42d83c9697db315
Vy Vo <52537680+isVy08@users.noreply.github.com>
2020-03-30 10:39:45 +1100

    Updated content

--- training.py
+++ training.py
@@ -9,7 +9,7 @@ Created on Sat Mar 28 00:46:15 2020
 import pandas as pd 
 from bs4 import BeautifulSoup
 from itertools import chain
-from langdetect import detect
+#from langdetect import detect
 
 data = pd.read_csv('dsjob_final.csv')
 
@@ -51,39 +51,54 @@ for element in train_data:
 train_data = list(chain(*train_data)) #unnest the list
 train_data = list(filter(lambda x: len(x) > 2 and '<strong>' in str(x[0]),train_data)) #filter data with bold title
 
-def labeling(data,jd_kw,jr_kw): 
+
+def labeling(data,jd_kw,jr_kw,html_content=False): 
     #1: Job responsibility, 2: Requirements, 3: Others 
-    # data is job description in html 
-    d = {'Content':[],'Label':label} 
+    # data is job description in html with each element is a list / paragraph 
+    d = {'Content':[],'Label':[]} 
     for inst in data: 
         #get content in raw text 
-        content = list(map(lambda x: x.get_text(),inst))
-        content = '.'.join(content) #join all elements in the list
-        d['Content'].append(content)
-        
+        if html_content: #want to convert html to text 
+            content = list(map(lambda x: x.get_text(),inst))
+            content = '.'.join(content) #join all elements in the list
+            d['Content'].append(content)
+            txt = inst[0].get_text().lower()
+        else: 
+            txt = inst[0].lower() 
         #get labels
-        txt = inst[0].get_text().lower() 
         i = 0
         j = 0
         result = False
-        while result == False and j < len(jr_kw): 
-            if i >= len(jd_kw):
-                result = all(s in txt for s in jr_kw[j].split(' '))
+        while result == False and j < len(jd_kw): 
+            if i >= len(jr_kw):
+                result = all(s in txt for s in jd_kw[j].split(' '))
                 j +=1 
             else: 
-                result = all(s in txt for s in jd_kw[i].split(' '))
+                result = all(s in txt for s in jr_kw[i].split(' '))
                 i+=1 
-        if i < len(jd_kw): 
-            d['Label'].append(1)
-        elif i >= len(jd_kw) and j < len(jr_kw): 
+        if i < len(jr_kw): 
             d['Label'].append(2)
+        elif i >= len(jr_kw) and j < len(jd_kw): 
+            d['Label'].append(1)
         else: 
             d['Label'].append(3)
     return d
 
+jd_kw = pd.read_csv('job_description.csv')
+jr_kw = pd.read_csv('job_requirement.csv')
+jd_kw, jr_kw = jd_kw.keyword, jr_kw.keyword 
 
+#read data
+df = pd.read_csv('englabel_nodup.csv')
+#split back to list 
+df['Raw'] = df.Content.map(lambda x: x.split('.'))
 
-#Adjusted data
-newdf = pd.read_csv('englabel_nodup.csv')
-newdf.info()
+d = labeling(df.Raw,jd_kw,jr_kw,html_content=False)
+df['New Label'] = d['Label']
+
+df[df['New Label']!=df['Label']].Label.value_counts()
+df.to_csv('englabel_nodup.csv',index=False)
+
+#check dups 
+df.drop(df[df.duplicated()].index,axis=0,inplace=True)
 
