9a81636ed28e9cf8e5e33159927488f404e1503d
Vy Vo <52537680+isVy08@users.noreply.github.com>
2020-04-03 01:51:30 +1100

    Edit files

--- preprocess.py
+++ preprocess.py
@@ -9,8 +9,7 @@ import re
 import pandas as pd 
 import numpy as np
 import pycountry as pcy
-import unicodedata
-from googletrans import Translator
+
 import ast
 
 pd.set_option('display.max_columns',100)
@@ -18,7 +17,7 @@ pd.set_option('display.max_rows',500)
 
 
 #1. read dataset
-data = pd.read_csv('dsjob_final.csv')
+data = pd.read_csv('datafull.csv')
 
 
 #2.Remove records where description = 0 & set other '0' as null values  
@@ -34,14 +33,14 @@ data['iso'] = data['link'].str.extract(r'https://([a-z]+)')
 data['iso'] = data['iso'].map(lambda x: 'us' if x=='www' else x)
 
 def to_country(x):
-    if x == 'www':
+    if x == 'us':
         return 'United States'
     elif x == 'uk':
         return 'United Kingdom'
     else: 
         return (pcy.countries.get(alpha_2=x.upper()).name)
 
-data['Country'] = data['Country'].apply(to_country)
+data['Country'] = data['iso'].apply(to_country)
 
 #extract Area (State / City)  from Location 
 
@@ -70,6 +69,7 @@ def to_area(x):
 loc = loc.map(lambda x: x[-1])   
 data['Area'] = loc.apply(to_area)
 
+
 #4. EXP LEVEL/ JOB FUNC / INDUSTRY / JOB TYPE
 
 #convert to List & get unique values for JOB FUNC / INDUSTRY 
@@ -77,49 +77,64 @@ data['Area'] = loc.apply(to_area)
 data['job_func'] = data['job_func'].map(lambda x: ast.literal_eval(x) if x is not np.nan else x)
 data['industry'] = data['industry'].map(lambda x: ast.literal_eval(x) if x is not np.nan else x)
 
-func = []
-inds = []
-
+fucins = {}
 for i in range(nrow):
-   try:
-       func.extend(data['job_func'][i])
-       inds.extend(data['industry'][i])
-   except:
-       pass             
+    try:
+        country = data['Country'][i]
+        if country in fucins.keys():        
+            fl = list(filter(lambda x: x not in fucins[country],data['job_func'][i]))
+            il = list(filter(lambda x: x not in fucins[country],data['industry'][i]))
+            fl.extend(il)
+            fucins[data['Country'][i]].extend(fl)
+        else: 
+            fucins[country] = []
+    except: 
+        pass            
 
-
-#detect language 
+# extract file to be translated 
 
 foreign = ['China','Japan','France','Germany','Italy','Netherlands','Spain','Sweden','Switzerland']
-d = {}
 for country in foreign: 
-    try:
-        d[country] = []
-        df = data[data.Country == country]
-        for col in ['exp_level','job_func','job_type','industry']:
-            try:
-                d[country].extend(df[col].unique())
-            except: 
-                for val in df[col]:
-                    l = list(filter(lambda x: x not in d[country],val))
-                    d[country].extend(l)
-    except:  
-       pass
+    df = data[data.Country == country]
+    text = []
+    for raw in [df['exp_level'].unique(),df['job_type'].unique(),fucins[country]]:
+        text.extend(raw)
+    filename = country+'.xlsx'
+    get_file(text,filename,country)            
+    
 
+# get translated results 
+
+original = {}
 for country in foreign: 
-    name = country+'.xlsx'
-    pd.DataFrame(d[country]).to_excel(name,index=False)
-
-
-#Translate to English using Google API (but with Limit!)   
-def to_eng(x):
-    trans = Translator()
-    try: 
-        return(trans.translate(x,dest='en').text)
-    except:
-        return(np.nan)
-
-
+    path = '/Users/vyvo/raw text/'+country+'.xlsx'
+    file = pd.read_excel(path)
+    original[country] = file[0]
+    
+trans = pd.read_excel('translated.xlsx')
  
+#form dictionary of 'original:translated'
+dictionary = {}
 
+for country in foreign: 
+    trs = trans[country].dropna()
+    if len(trs) != len(original[country]):
+        print('Unmatched length for',country)
+        pass 
+    else: 
+        for i in range(len(trs)):
+            dictionary[original[country][i]] = trs[i]
 
+#normalize values in dictionary 
+
+     
+# translate 
+for col in ['exp_level','job_type','job_func','industry']:
+    new = col+'_v2'
+    if col in ['exp_level','job_type']:         
+        data[new] = data[col].map(lambda x: translate(x) if x in dictionary.keys() else x)
+    else: 
+        data[new] = data[col].map(lambda x: [translate(i) for i in x] if isinstance(x,list) else x)
+         
+test = data.dropna()
+idx = test[test.non_English==1].index
