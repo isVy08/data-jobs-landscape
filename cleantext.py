d5b9ce91144dbe253fd8d4b79c42050f545f4e41
Vy Vo <52537680+isVy08@users.noreply.github.com>
2020-04-06 22:00:45 +1000

    Edit files

--- cleantext.py
+++ cleantext.py
@@ -6,9 +6,6 @@ Created on Wed Apr  1 21:21:32 2020
 @author: vyvo
 """
 
-import unicodedata 
-from googletrans import Translator
-from bs4 import BeautifulSoup
 
 
 #Remove accented characters 
@@ -20,6 +17,20 @@ def remove_special_characters(text, remove_digits=False):
     pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
     text = re.sub(pattern, ' ', text)
     return text
+
+def remove_stopwords(text, is_lower_case=False,return_str=True):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    if is_lower_case:
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+    else:
+        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    if return_str:
+        filtered_text = ' '.join(filtered_tokens)
+        return filtered_text
+    else: 
+        return filtered_tokens
+    
 
 def remove_html(text):
     parse = BeautifulSoup(text,'lxml')
