34983ecf0e584124401eacd168759ff4e8c7e86f
Vy Vo <52537680+isVy08@users.noreply.github.com>
2021-06-27 11:17:48 +1000

    Initial Commit

--- cleantext.py
+++ cleantext.py
@@ -1,50 +1,68 @@
 from dependency import * 
 tokenizer = ToktokTokenizer()
-nltk.download('averaged_perceptron_tagger')
+
+def remove_html(text):
+  parse = BeautifulSoup(text,'lxml')
+  return parse.get_text(' ').lower()
 
 
 def remove_accented_chars(text):
-    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-    return text
+  text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+  return text
 
 def remove_special_characters(text, remove_digits=False):
-    pattern = re.compile('[^a-zA-z0-9.\s]') if not remove_digits else r'[^a-zA-z\s]'
-    text = re.sub(pattern, '', text)
-    return text
+  pattern = re.compile('[^a-zA-z0-9\s]') if not remove_digits else r'[^a-zA-z\s]'
+  text = re.sub(pattern, ' ', text)
+  return text
 
-
-
-nltk.download('stopwords')
-stopword_list = nltk.corpus.stopwords.words('english')
-def remove_stopwords(text, is_lower_case=False,return_str=True):
-    tokens = tokenizer.tokenize(text)
-    tokens = [token.strip() for token in tokens]
-    if is_lower_case:
-        filtered_tokens = [token for token in tokens if token not in stopword_list]
-    else:
-        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
-    if return_str:
-        filtered_text = ' '.join(filtered_tokens)
-        return filtered_text
-    else: 
-        return filtered_tokens
+def remove_stopwords(text, is_lower_case=False):
+  nltk.download('stopwords')
+  stopword_list = nltk.corpus.stopwords.words('english')
+  
+  tokens = tokenizer.tokenize(text)
+  tokens = [token.strip() for token in tokens]
+  if is_lower_case:
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+  else:
+    filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+  
+  filtered_text = ' '.join(filtered_tokens)
+  return filtered_text
     
 
-def remove_html(text):
-    parse = BeautifulSoup(text,'lxml')
-    return parse.get_text(' ').lower()
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": 'a',
+                "N": 'n',
+                "V": 'v',
+                "R": 'r'}
+    return tag_dict.get(tag)
 
 
-nltk.download('wordnet')
-def lemmatize_text(text,pos='n'): 
-    w = WordNetLemmatizer()
-    tokens = tokenizer.tokenize(text)
-    tokens = [token.strip() for token in tokens] #remove space
-    filtered_tokens = [w.lemmatize(token,pos) for token in tokens ]
-    filtered_text = ' '.join(filtered_tokens)
-    return filtered_text 
+def wordnet_lemmatize(text): 
+  from nltk.stem import WordNetLemmatizer
+  nltk.download('wordnet')
+  nltk.download('averaged_perceptron_tagger')
+  w = WordNetLemmatizer()
+  
+  tokens = tokenizer.tokenize(text)
+  tokens = [token.strip() for token in tokens]
+  lemma_tokens = [w.lemmatize(token, get_wordnet_pos(token)) for token in tokens]
+  
+  lemma_text = ' '.join(lemma_tokens)
+  return lemma_text 
+
+
+def spacy_lemmatize(text): 
+  import spacy  
+  nlp = spacy.load('en', disable=['parser', 'ner'])
+  lemma_text = nlp(text)
+  lemma_text = " ".join([token.lemma_ for token in lemma_text])
+  return lemma_text 
     
-def quick_clean_text(text,html=1, specchar=1, stopword=1,lemmatize=1): 
+
+def quick_clean(text, html=1, specchar=1, stopword=1, lemmatize=1): 
     if html == 1:
         text = remove_html(text)
     if specchar == 1: 
@@ -52,6 +70,7 @@ def quick_clean_text(text,html=1, specchar=1, stopword=1,lemmatize=1):
     if stopword == 1:
         text = remove_stopwords(text)
     if lemmatize == 1:
-        text = lemmatize_text(text)
-    return text 
-
+        text = wordnet_lemmatize(text)
+    if lemmatize == 2:
+        text = spacy_lemmatize(text)
+    return text
--- dependency.py
+++ dependency.py
@@ -4,16 +4,16 @@ import pandas as pd
 import numpy as np 
 import scipy
 import os
-#import pycountry as pcy
+import pycountry as pcy
 from itertools import chain
 
 # Webdriver & HTML parsing 
 import requests
 from bs4 import BeautifulSoup
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# NLP
+# Text processing
 import re
 import unicodedata
 import nltk
@@ -21,24 +21,6 @@ from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.util import ngrams
 
-# Gensim
-from gensim.models import word2vec, doc2vec
-from gensim.models.callbacks import CallbackAny2Vec
-
-
-# Classifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import plot_confusion_matrix
-from sklearn import preprocessing
-
-# visualize embeddings
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt 
 
 # Adjust display settings 
 pd.set_option('display.max_columns',500)
